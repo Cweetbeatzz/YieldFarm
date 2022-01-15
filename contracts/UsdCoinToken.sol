c34f19f5047583aad09845fa277e71fc4069e730
Cweetbeatzz <Cweetbeatz@gmail.com>
2022-01-15 23:07:44 +0100

    advanced features added

--- contracts/UsdCoinToken.sol
+++ contracts/UsdCoinToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract TrueUsdToken is
+contract UsdCoinToken is
     ERC20,
     ERC20Burnable,
     ERC20Snapshot,
--- contracts/YieldFarm.sol
+++ contracts/YieldFarm.sol
@@ -1,11 +1,14 @@
 //SPDX-License-Identifier:MIT
 
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity ^0.8.0;
 
 import "./SimbaToken.sol";
 import "./DaiToken.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+// This interface represents all tokens that will be added to this Defi Application
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract YieldFarm {
+contract YieldFarm is Ownable {
     //deploy dai token
     //deploy simba token
     //minimum staking fee
@@ -18,26 +21,37 @@ contract YieldFarm {
     uint256 minimumStakeAmount;
     address[] public stakers;
     string public name = "Yield Farming";
-    SimbaToken public simbaToken;
-    DaiToken public daiToken;
-    address public owner;
+    IERC20 public simbaToken;
+    IERC20 public geminiDollar;
+    IERC20 public sUsd;
+    IERC20 public susdt;
+    IERC20 public usdcoin;
+    IERC20 public daiToken;
+    address[] public allowedTokens;
     // ############################################################################
 
-    mapping(address => uint256) public stakedAmount;
+    mapping(address => mapping(address => uint256)) public stakedAmount;
     mapping(address => bool) public hasStaked;
     mapping(address => bool) public isStaking;
+    mapping(address => uint256) public unique_Token_staked;
+    mapping(address => address) public pricefeed;
 
     // ############################################################################
 
-    constructor(DaiToken _daitoken, SimbaToken _simbatoken) public {
-        daiToken = _daitoken;
-        simbaToken = _simbatoken;
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
+    constructor(
+        address _daitoken,
+        address _simbatoken,
+        address _geminiDollar,
+        address _sUsd,
+        address _susdt,
+        address _usdcoin
+    ) public {
+        daiToken = IERC20(_daitoken);
+        simbaToken = IERC20(_simbatoken);
+        geminiDollar = IERC20(_geminiDollar);
+        sUsd = IERC20(_sUsd);
+        susdt = IERC20(_susdt);
+        usdcoin = IERC20(_usdcoin);
     }
 
     // ############################################################################
@@ -51,15 +65,18 @@ contract YieldFarm {
 
     // ############################################################################
 
-    function Stake(uint256 _amount) public {
+    function Stake(uint256 _amount, address _token) public {
+        require(AllowedTokenForStaking(_token), "Token is not allowed");
         //getting the minimum required fee to invest
         getMinStakeFee(_amount);
         //user deposits minimum amout of token required to invest to this contract
-        daiToken.transferFrom(msg.sender, address(this), _amount);
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        //calling function to check if users has multiple stakes in different tokens
+        uniqueToken(msg.sender, _token);
         //mapping for each user & how much he/she staked
-        stakedAmount[msg.sender] += _amount;
+        stakedAmount[_token][msg.sender] += _amount;
         //add users to list if they have not staked
-        if (!hasStaked[msg.sender]) {
+        if (unique_Token_staked[msg.sender] == 1) {
             stakers.push(msg.sender);
         }
         //get stake status
@@ -69,14 +86,37 @@ contract YieldFarm {
 
     // ############################################################################
 
+    function uniqueToken(address _user, address _token) internal {
+        if (stakedAmount[_token][_user] <= 0) {
+            unique_Token_staked += 1;
+        }
+    }
+
+    // ############################################################################
+    function AddTokens(address _token) public onlyOwner {
+        // only admin can add tokens
+        allowedTokens.push(_token);
+    }
+
+    // ############################################################################
+
+    function AllowedTokenForStaking(address _token) public returns (bool) {
+        //only tokens listed can be staked on
+        for (uint256 i = 0; i < allowedTokens.length; i++) {
+            if (allowedTokens[i] == _token) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // ############################################################################
+
     function unStake(uint256 amount) public {}
 
     // ############################################################################
 
     function AwardIntrests() public onlyOwner {
-        //only owner can call this function
-        // require(msg.sender == owner, "Caller must be the Owner");
-
         //looping through all stakers list
 
         for (uint256 i = 0; i < stakers.length; i++) {
@@ -87,8 +127,34 @@ contract YieldFarm {
             //sending rewards only if the balance from the array shows that the user has staked so
             //if less than or equal to zero they have not staked
             if (bal > 0) {
-                simbaToken.transfer(user, bal);
+                if (daiToken) {
+                    daiToken.transfer(user, bal);
+                }
+                if (simbaToken) {
+                    simbaToken.transfer(user, bal);
+                }
+                if (geminiDollar) {
+                    geminiDollar.transfer(user, bal);
+                }
+                if (sUsd) {
+                    sUsd.transfer(user, bal);
+                }
+                if (susdt) {
+                    susdt.transfer(user, bal);
+                }
+                if (usdcoin) {
+                    usdcoin.transfer(user, bal);
+                }
             }
         }
+    }
+
+    // ############################################################################
+
+    function getTokenPriceFeed(address _token, address _pricefeed)
+        public
+        onlyOwner
+    {
+        pricefeed[_token] = _pricefeed;
     }
 }
